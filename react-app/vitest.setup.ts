583bbd69218551a444801a22907d22787ae491e1
ilyabulya <ilyade3004@gmail.com>
2025-03-19 10:54:00 +0100

    Test Husky with staged files

--- react-app/vitest.setup.ts
+++ react-app/vitest.setup.ts
@@ -1,4 +1,4 @@
 import { expect } from "vitest";
-import * as matchers from "@testing-library/jest-dom/matchers";
+import matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
